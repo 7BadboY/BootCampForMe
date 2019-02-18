18e460aadf58753cb718002d953533332bccc611
Yarotskiy Sergey <abrawa@gmail.com>
2019-02-18 17:31:03 +0200

    add new dir and home task

---
+++ js_m01d01ht/js/script.js
@@ -0,0 +1,64 @@
+// // // // // // // let guest;
+// // // // // // // let name;
+
+// // // // // // // name = 'Mango';
+// // // // // // // guest = name;
+// // // // // // // console.log(guest);
+
+
+
+// // // // // // const day = 02;
+// // // // // // const month = 10;
+// // // // // // const year = 2017;
+
+// // // // // // const date = " 0" + day + "\\" + month + "\\" + year;
+// // // // // // console.log(date); 
+
+
+
+// // // // // const name = prompt('enter your name')
+// // // // // alert (name);
+
+
+// // // // const name = 'Mango';
+// // // // const date = '14/08/2031';
+// // // // const roomType = 'lux'
+
+// // // // const message = `${name} coming to the rest ${date} in ${roomType}`;
+
+// // // // console.log(message);
+
+// // // const padding = "20px";
+// // // const border = "5px";
+// // // const contentWidth = "100px";
+
+// // // let totalWidth;
+// // // totalWidth = Number.parseInt(padding) + Number.parseInt(border) + Number.parseInt(contentWidth) + "px";
+
+// // // console.log(totalWidth);
+
+
+// // const year = +prompt('What year now?');
+
+// // if (year === 2018) {
+// //     alert('all good');
+// // } else {
+// //     alert("it's not true");
+// // }
+
+
+// const value = +prompt('введите целое произвольное');
+// console.log(value);
+
+// if (value === null) {
+
+//     alert ('приходите ещё!')
+// }
+// else if (Number.isInteger(Number(value))) {
+//     alert('спасибо')   
+// }
+// else {
+//     alert('Необходимо ввести целое число')
+// }
+
+
