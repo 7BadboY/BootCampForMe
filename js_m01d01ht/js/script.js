4ca0b1d218aab00075258bfde56c8cc053e8cfc5
Yarotskiy Sergey <abrawa@gmail.com>
2019-02-19 06:11:40 +0200

    Trying to do Home task

--- js_m01d01ht/js/script.js
+++ js_m01d01ht/js/script.js
@@ -1,3 +1,39 @@
+'use strict'
+
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+
+const adminLoginAccessSkiped = 'Отменено пользователем!';
+const adminLoginAccessWrong = 'Доступ запрещен, неверный логин!';
+
+const adminPasswordAccessSkiped = 'Отменено пользователем!';
+const adminPasswordAccessWrong = 'Доступ запрещен, неверный пароль!';
+
+const adminAccessGreat = 'Добро пожаловать!';
+
+const login = prompt('enter your login');
+
+if (login === null) {
+    alert (adminLoginAccessSkiped);
+} else if (login !== adminLogin) {
+    alert (adminLoginAccessWrong);
+} else {
+    const password = prompt('enter your password');
+
+if (password === null) {
+    alert (adminPasswordAccessSkiped);
+} else if (password !== adminPassword) {
+    alert (adminPasswordAccessWrong);
+} else {
+    alert (adminAccessGreat);
+ }
+} 
+
+
+
+
+
+
 // // // // // // // let guest;
 // // // // // // // let name;
 
@@ -62,3 +98,55 @@
 // }
 
 
+
+// const num = Number.parseInt(Math.random() * 100);
+
+// let type;
+
+// console.log('num',num);
+
+// switch (num % 2) {
+//     case 0: 
+//     type = "even";
+//     break;
+//     default:
+//     type = "odd";
+// }
+
+// console.log(`${num} is ${type}`);
+
+
+// let hours = 7;
+// let minutes = 3;
+// let seconds = 42;
+
+// const time =`${hours < 10 ? 
+// '0' + hours : hours}:${minutes < 10 ? 
+// '0' + minutes : minutes}:${seconds < 10 ? 
+// '0' + seconds : seconds}`;
+
+// console.log('Time is: ' , time);
+
+
+// const promptValue = prompt("Type number from 1-5");
+
+// if (Number.isInteger(+promptValue) && (promptValue > 0 && promptValue <= 5) ) {
+//     switch(+promptValue) {
+//         case 1 :
+//         alert ("Каталог хостелов")
+//         break;
+//         case 2 :
+//         alert ("Каталог отелей ***")
+//         break;
+//         case 3 :
+//         alert ("Каталог отелей ****")
+//         break;
+//         case 4 :
+//         alert ("Каталог лучших отелей")
+//         break;
+//     }
+// } else if (promntValue === null) {
+//     alert ("очень жаль, приходите еще!")
+// } else {
+//     alert ("Неверный ввод, возможные варианты 1-5!")
+// }
